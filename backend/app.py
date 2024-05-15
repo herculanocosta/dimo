e832e0ca7af34ff36b95766e7d269e713a3b9562
Herculano <herculano.esl@gmail.com>
2024-05-15 23:20:47 +0100

    update flask app to use environment port

--- backend/app.py
+++ backend/app.py
@@ -1,14 +1,16 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": "*"}})  # Enable CORS for all routes
+CORS(app)  # Enable CORS for all routes
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
     data = request.json
-    print("Received data:", data)  # Add a print statement for debugging
+    print("Received data:", data)
     return jsonify(data)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT environment variable or default to 5000
+    app.run(host='0.0.0.0', port=port)
